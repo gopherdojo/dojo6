0c05d0003dac012f19e969506d89c07a8ab2df82
suaaa7 <spuaaa7@gmail.com>
2019-07-15 18:56:21 +0900

    Add pkg/convdir

---
+++ kadai1/pei/pkg/convdir/convdir_test.go
@@ -0,0 +1,30 @@
+package convdir_test
+
+import (
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/gopherdojo/dojo6/kadai1/pei/pkg/convdir"
+	"github.com/gopherdojo/dojo6/kadai1/pei/pkg/imgconv"
+)
+
+func TestConverterWithDir_Convert(t *testing.T) {
+	cd := convdir.ConverterWithDir{
+		Dir: "../../testdata",
+		InputExtension: imgconv.JPEG,
+		OutputExtension: imgconv.PNG,
+		LeaveInput: true,
+	}
+	got := cd.Convert()
+	want := []convdir.ConvertedResult{
+		{OutputPath: "../../testdata/sample.png"},
+		{OutputPath: "../../testdata/testdir1/testdir2/sample.png"},
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+
+	os.Remove(want[0].OutputPath)
+	os.Remove(want[1].OutputPath)
+}
