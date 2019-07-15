b7c9643d30516c0bb73b55d0d535f4b989eb8579
annkara <howtobecomeahacker@gmail.com>
2019-07-15 18:47:24 +0900

    1. オプションの変更に伴い失敗していたテストコードの修正 ディレクトリ名を渡してしまうとテストに失敗してしまうので、オプションの解析のみテスト実施

--- kadai1/annkara/cmd/i2i/cli_test.go
+++ kadai1/annkara/cmd/i2i/cli_test.go
@@ -13,23 +13,19 @@ func TestRun(t *testing.T) {
 		expectedStatus int
 	}{
 		{
-			desc:           "正常終了",
+			desc:           "正常終了 : コマンド名のみを指定",
 			arg:            "i2i",
 			expectedStatus: exitCodeOK,
 		}, {
-			desc:           "正常終了",
-			arg:            "i2i -a jpeg -t png directory",
+			desc:           "正常終了 : ショートオプションを指定",
+			arg:            "i2i -b jpeg -a png",
 			expectedStatus: exitCodeOK,
 		}, {
-			desc:           "正常終了",
-			arg:            "i2i -after jpeg -target png directory",
+			desc:           "正常終了 : ロングオプションを指定",
+			arg:            "i2i -before jpeg -after png",
 			expectedStatus: exitCodeOK,
 		}, {
-			desc:           "正常終了",
-			arg:            "i2i directory",
-			expectedStatus: exitCodeOK,
-		}, {
-			desc:           "異常終了",
+			desc:           "異常終了：不正なオプションを指定",
 			arg:            "i2i -u unkonwn",
 			expectedStatus: exitCodeErr,
 		},
@@ -42,7 +38,7 @@ func TestRun(t *testing.T) {
 		args := strings.Split(tc.arg, " ")
 		status := cli.run(args)
 		if status != tc.expectedStatus {
-			t.Errorf("desc: Success, status should be %v, not %v", exitCodeOK, status)
+			t.Errorf("desc: %v, status should be %v, not %v", tc.desc, tc.expectedStatus, status)
 		}
 	}
 }
