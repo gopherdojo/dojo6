22681712941c3fa485bcd247b37a0adb930b1c27
Kobayashi Shota <annkara@Kobayashi-no-MacBook-Air.local>
2019-07-15 13:39:00 +0900

    環境依存を避けるため、strings.Joinを利用

--- kadai1/annkara/pkg/image/convertor_test.go
+++ kadai1/annkara/pkg/image/convertor_test.go
@@ -2,19 +2,20 @@ package image
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
 
-var path = "../../test/"
+var path = filepath.Join("..", "..", "test")
 
 func TestConvertSuccess(t *testing.T) {
-	in, err := os.Open(path + "test.jpg")
+	in, err := os.Open(filepath.Join(path, "test.jpg"))
 	if err != nil {
 		t.Fatalf("failed test %#v", err)
 	}
 	defer in.Close()
 
-	out, err := os.Create(path + "test.png")
+	out, err := os.Create(filepath.Join(path, "test.png"))
 	if err != nil {
 		t.Fatalf("failed test %#v", err)
 	}
