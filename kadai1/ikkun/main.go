baf96720d52f23e264202328f97fa50b4005b038
isuzuki <ikkuntech@gmail.com>
2019-07-17 01:56:17 +0900

    added test

--- kadai1/ikkun/main.go
+++ kadai1/ikkun/main.go
@@ -3,32 +3,48 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 
 	"github.com/gopherdojo/dojo6/kadai1/ikkun/imgconv"
 )
 
+const (
+	ExitOK = iota
+	ExitError
+)
+
+type CLI struct {
+	outStream io.Writer
+	errStream io.Writer
+}
+
 func main() {
+	c := &CLI{os.Stdout, os.Stderr}
+	os.Exit(c.run())
+}
+
+func (c *CLI) run() int {
 	from := flag.String("f", "jpg", "from format(jpg|jpeg|png|gif) default jpg.")
 	to := flag.String("t", "png", "to format(jpg|jpeg|png|gif) default png.")
 
 	flag.Parse()
 
 	if !validateFormat(*from) {
-		fmt.Fprintln(os.Stderr, fmt.Sprintf("unsupport format: %s", *from))
-		os.Exit(1)
+		fmt.Fprintln(c.errStream, fmt.Sprintf("unsupport format: %s", *from))
+		return ExitError
 	}
 
 	if !validateFormat(*to) {
-		fmt.Fprintln(os.Stderr, fmt.Sprintf("unsupport format: %s", *to))
-		os.Exit(1)
+		fmt.Fprintln(c.errStream, fmt.Sprintf("unsupport format: %s", *to))
+		return ExitError
 	}
 
 	args := flag.Args()
 	if len(args) < 1 {
-		fmt.Fprintln(os.Stderr, "required directory assigned")
-		os.Exit(1)
+		fmt.Fprintln(c.errStream, "required directory assigned")
+		return ExitError
 	}
 
 	dir := args[0]
@@ -45,17 +61,20 @@ func main() {
 		return nil
 	})
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		fmt.Fprintln(c.errStream, err)
+		return ExitError
 	}
 
 	for _, path := range paths {
 		img := imgconv.ImageFile{path, *from, *to}
 		err = imgconv.Convert(img)
 		if err != nil {
-			fmt.Fprintf(os.Stdout, "%s: %s\n", err, img.Source)
+			// 対象ファイル外が含まれている場合は、標準出力するが処理自体は止めない
+			fmt.Fprintf(c.outStream, "%s: %s\n", err, img.Source)
 		}
 	}
+
+	return ExitOK
 }
 
 func validateFormat(f string) bool {
