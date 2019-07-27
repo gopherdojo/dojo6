29b2d0dca8a440901786fb0f846ff1817a9e08fc
annkara <howtobecomeahacker@gmail.com>
2019-07-27 20:16:52 +0900

    convertor_test.goの修正
    テーブル駆動テスト形式に修正。
    テストヘルパーを利用する形に修正したが、異常系のテストがまだ実施できていない。

--- kadai2/annkara/pkg/image/convertor_test.go
+++ kadai2/annkara/pkg/image/convertor_test.go
@@ -6,24 +6,59 @@ import (
 	"testing"
 )
 
-var path = "testdata"
+var directory = "testdata"
 
-func TestConvertSuccess(t *testing.T) {
+type testcase struct {
+	desc     string
+	origin   string
+	target   string
+	format   string
+	expected string
+}
 
-	in, err := os.Open(filepath.Join(path, "test.jpg"))
+func TestConvert(t *testing.T) {
+
+	tests := []testcase{
+		{
+			desc:   "jpg to png",
+			origin: filepath.Join(directory, "test.jpg"),
+			target: filepath.Join(directory, "test2.png"),
+			format: "png",
+		}, {
+			desc:   "png to jpg",
+			origin: filepath.Join(directory, "test.png"),
+			target: filepath.Join(directory, "test2.jpg"),
+			format: "jpg",
+		}, {
+			desc:   "Unknow format",
+			origin: filepath.Join(directory, "test.png"),
+			target: filepath.Join(directory, "test2.diff"),
+			format: "diff",
+		},
+	}
+
+	for _, tc := range tests {
+		testConvert(t, tc)
+	}
+}
+
+func testConvert(t *testing.T, tc testcase) {
+	in, err := os.Open(tc.origin)
 	if err != nil {
-		t.Fatalf("failed test %#v", err)
+		t.Fatalf("failed test %s: %#v", tc.desc, err)
 	}
 	defer in.Close()
 
-	out, err := os.Create(filepath.Join(path, "test.png"))
+	out, err := os.Create(tc.target)
 	if err != nil {
-		t.Fatalf("failed test %#v", err)
+		t.Fatalf("failed test %s: %#v", tc.desc, err)
 	}
+	defer os.Remove(tc.target)
 	defer out.Close()
 
-	err = Convert(in, out, "jpg")
+	err = Convert(in, out, tc.format)
 	if err != nil {
-		t.Fatalf("failed test %#v", err)
+		t.Fatalf("failed test %s: %#v", tc.desc, err)
 	}
+
 }
