953d94542258f2d53dc661a719b6c2a7cbd619a2
en-ken <inonakano.kawaz.taikai.never.knows@gmail.com>
2019-07-31 17:56:21 +0900

    refactor: chage my algo to filepath.Walk

--- kadai2/en-ken/imgcnv/dirpath.go
+++ kadai2/en-ken/imgcnv/dirpath.go
@@ -1,45 +1,40 @@
 package imgcnv
 
 import (
-	"io/ioutil"
+	"os"
 	"path/filepath"
 )
 
 // IAllFilePaths is I/F of AllFilePaths
 type IAllFilePaths func(path string, ext string) ([]string, error)
 
+const allocSize = 100
+
 // AllFilePaths returns
 // the paths of the files in the specified directory
 // filtered by the specified extension.
 func AllFilePaths(path string, ext string) ([]string, error) {
-	absPath, err := filepath.Abs(path)
-	if err != nil {
-		return nil, err
-	}
 
-	return searchFiles(absPath, ext)
-}
-
-func searchFiles(dirPath string, ext string) ([]string, error) {
-
-	files, err := ioutil.ReadDir(dirPath)
-	if err != nil {
-		return nil, err
-	}
-
-	result := make([]string, 0) // allocate 0 items to merge 2 slices
-	for _, file := range files {
-		path := filepath.Join(dirPath, file.Name())
-		if file.IsDir() {
-			paths, _ := searchFiles(path, ext)
-			result = append(result, paths...)
-		} else {
-			// ignore error
-			isMatch, _ := filepath.Match("*."+ext, file.Name())
-			if isMatch {
-				result = append(result, path)
-			}
+	result := make([]string, 0, allocSize)
+	err := filepath.Walk(path, func(filePath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
 		}
-	}
-	return result, nil
+
+		if info.IsDir() {
+			// Skip
+			return nil
+		}
+
+		// Find by extension
+		isMatch, _ := filepath.Match("*."+ext, info.Name())
+		if isMatch {
+			absPath, err := filepath.Abs(filePath)
+			result = append(result, absPath)
+			return err
+		}
+		return nil
+	})
+
+	return result, err
 }
