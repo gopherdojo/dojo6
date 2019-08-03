ee1751592608adf73f2a752a8c53d7ecf6c04c1f
roshiyak <yashiro.kentaro@icloud.com>
2019-08-03 17:22:27 +0900

    add comment

--- kadai3-1/yashiken/typinggame.go
+++ kadai3-1/yashiken/typinggame.go
@@ -13,6 +13,7 @@ import (
 var t int
 var s string
 
+// フラグのパース
 func init() {
 	flag.IntVar(&t, "t", 1, "Time limit")
 	flag.StringVar(&s, "s", "wordlist.txt", "wordlist")
@@ -25,21 +26,28 @@ func main() {
 		score = 0
 		chrcv = input(os.Stdin)
 	)
+	// 単語リストの取得
 	words, err := getWords(s)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	// 単語リストの要素をランダムに入れ替え
+	words = shuffle(words)
 
 	fmt.Printf("タイピングゲームを始めます。制限時間は%d分です。\n", t)
 	for i := true; i && score < len(words); {
 		qst := words[score]
 		fmt.Println(qst)
 		select {
+		// チャネルを通して標準入力から文字列を受け取った場合、
+		// 正誤判定を行い、正解ならscoreをインクリメント
 		case ans := <-chrcv:
 			if qst == ans {
 				score++
 			}
+		// チャネルを通して制限時間の終了を通知されたとき、
+		// ループを脱出
 		case <-tm:
 			fmt.Println("制限時間です。ゲームを終了します。")
 			i = false
@@ -48,6 +56,8 @@ func main() {
 	fmt.Printf("あなたの正解数は%d問です。", score)
 }
 
+// getWordsは指定したファイルの各行の文字列をスライスに
+// 格納して返します。
 func getWords(s string) ([]string, error) {
 	file, err := os.Open(s)
 	if err != nil {
@@ -65,11 +75,11 @@ func getWords(s string) ([]string, error) {
 		return nil, err
 	}
 
-	sl = shuffle(sl)
-
 	return sl, nil
 }
 
+// inputは新たなゴルーチンを生成し、そのゴルーチンが
+// 標準入力の内容を受け取ったあとチャネルに格納して返します。
 func input(r io.Reader) <-chan string {
 	ch := make(chan string)
 	go func() {
@@ -81,6 +91,8 @@ func input(r io.Reader) <-chan string {
 	return ch
 }
 
+// shuffleはスライスの要素をランダムに入れ替え、
+// 要素を入れ替えたスライスを返します。
 func shuffle(s []string) []string {
 	rand.Seed(time.Now().UnixNano())
 	for i := range s {
