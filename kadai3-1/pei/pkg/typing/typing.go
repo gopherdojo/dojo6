d27876c6ea433cd0af93ce8979dd9dda80df0423
suaaa7 <spuaaa7@gmail.com>
2019-08-06 02:43:34 +0900

    Add Typing Game

---
+++ kadai3-1/pei/pkg/typing/typing.go
@@ -0,0 +1,34 @@
+package typing
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func Question(words []string) <-chan bool {
+	isCorrectCh := make(chan bool)
+
+	go func() {
+		stdin := bufio.NewScanner(os.Stdin)
+		rand.Seed(time.Now().UnixNano())
+
+		for {
+			word := words[rand.Intn(len(words))]
+			fmt.Println(word)
+
+			stdin.Scan()
+			answer := stdin.Text()
+			if word == answer {
+				isCorrectCh <- true
+			} else {
+				isCorrectCh <- false
+			}
+		}
+		close(isCorrectCh)
+	}()
+
+	return isCorrectCh
+}
