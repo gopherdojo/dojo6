5a0a818db0f50187f2d7b8be907b2e45b44caa54
annkara <howtobecomeahacker@gmail.com>
2019-08-06 07:31:21 +0900

    初期実装追加

--- kadai3-1/annkara/pkg/gotyping/gotyping.go
+++ kadai3-1/annkara/pkg/gotyping/gotyping.go
@@ -1,19 +1,80 @@
 package gotyping
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
 	"io"
-	"log"
+	"math/rand"
+	"os"
+	"time"
 )
+
+type gotyping struct {
+	outStream io.Writer
+	errStream io.Writer
+	limits    time.Duration
+	result    int
+}
 
 // Run the gotyping
 func Run(outStream, errStream io.Writer) error {
 
-	log.SetOutput(errStream)
-
-	var limits int
-	flag.IntVar(&limits, "limits", 60, "制限時間")
+	var l int
+	flag.IntVar(&l, "limits", 60, "制限時間")
 	flag.Parse()
 
+	g := gotyping{
+		outStream: outStream,
+		errStream: errStream,
+		limits:    time.Duration(l) * time.Second,
+	}
+
+	err := g.start()
+
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func (g *gotyping) start() error {
+
+	rand.Seed(time.Now().Unix())
+	fmt.Fprintln(g.outStream, "=== gotyping start ===")
+
+	in := make(chan string)
+	go input(in)
+
+END:
+	for {
+		question := word()
+		fmt.Fprintln(g.outStream, fmt.Sprintf("> %v", question))
+
+		select {
+		case <-time.After(g.limits):
+			fmt.Fprintln(g.outStream, "=== gotyping finish ===")
+			break END
+		case answer := <-in:
+			if question == answer {
+				g.result++
+			}
+		}
+	}
+
+	fmt.Fprintf(g.outStream, "Score: %v\n", g.result)
+	return nil
+}
+
+func word() string {
+	words := []string{"go", "Java", "C", "ruby", "perl", "assmbler"}
+	return words[rand.Intn(len(words))]
+}
+
+func input(in chan string) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		in <- scanner.Text()
+	}
 }
